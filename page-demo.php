171fecc3431b78891e9c95aa8b2b371506552376
felixfranz <hello@felixfranz.com>
2026-04-22 11:52:53 +0200

    set up flex content

---
+++ page-demo.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: Demo
+
+ */
+/**
+ *
+ * @package New_Base
+ */
+
+get_header();
+?>
+
+	<main id="main" class="site-main page__site-main">
+
+		<?php 
+    
+    get_template_part( 'template-parts/content', 'flexible' );
+
+    ?>
+  
+
+	</main><!-- #main -->
+
+<?php 
+get_footer();
+ ?>
+
+
