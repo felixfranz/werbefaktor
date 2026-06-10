3531c25cc9daa37ac67332d4d4e7aac43f773302
felixfranz <hello@felixfranz.com>
2026-06-10 09:50:23 +0200

    Deploy Test

---
+++ page-kontakt.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Kontakt
+
+ */
+/**
+ *
+ * @package New_Base
+ */
+
+get_header();
+?>
+
+	<main id="main" class="site-main page__site-main">
+
+  <?php 
+    
+    get_template_part( 'template-parts/content', 'flexible_sections' );
+
+    ?>
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
