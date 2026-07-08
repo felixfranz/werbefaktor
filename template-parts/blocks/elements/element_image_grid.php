4581592d6fc1b20f58b5e7a36584f3f1d0d25cb7
felixfranz <hello@felixfranz.com>
2026-07-08 09:59:32 +0200

    grid fix

--- template-parts/blocks/elements/element_image_grid.php
+++ template-parts/blocks/elements/element_image_grid.php
@@ -12,12 +12,21 @@
                        <div  class="image_grid" >
                          
                                 <?php foreach ($gallery as $image) : 
-                                    
+                                    $content .= '<div class="grid_image">';
+                                    $content .= '<a class="gallery_image" data-slb-group="lighbox-2" href="'. $image['url'] .'">'; 
+                                    $content .= '<img src="'. esc_url($image['sizes']['large']) .'" data-slb-group="lighbox-1">';
+                                    $content .= '</a></div>';
                                     ?>
-                                    <div class="grid_image">
-                                        <img src="<?php echo esc_url($image['sizes']['large']); ?>">
-                                    </div>
-                                <?php endforeach; // end Bilder ?>
+                                    
+                                <?php endforeach; // end Bilder 
+                                // start Lightbox
+                                if ( function_exists('slb_activate') ){
+                                $content = slb_activate($content);
+                                }
+                                    
+                                echo $content;
+                                 ?>
+                                
 
                             </div>
                            
